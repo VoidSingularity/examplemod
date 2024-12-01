f6398a92d072e8a269788cbd4b3da7c406b56de3
Azure Bluet <azure@escalopia.net>
2024-12-01 20:35:17 +0800

    Update build system

--- src/main/berry/test/mixins/SlimeGlitchUnfix.java
+++ src/main/berry/test/mixins/SlimeGlitchUnfix.java
@@ -3,7 +3,6 @@ package berry.test.mixins;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.core.BlockPos;
